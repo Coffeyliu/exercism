ccb6de0386c03e6ac72e53a7aa3619237f53e972
Coffeyliu <taliu@wiley.com>
2020-01-28 14:00:25 -0500

    modify solution -- avoid repeating the code

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,15 +1,13 @@
 class Matrix:
     def __init__(self, matrix_string):
-        self.matrix_string = matrix_string
+        self.matrix_string_list = matrix_string.split('\n')
 
     def row(self, index):
-        list_matrix = self.matrix_string.split('\n')
-        res = list_matrix[index-1].split()
+        res = self.matrix_string_list[index-1].split(' ')
         return [int(num) for num in res]
 
     def column(self, index):
-        list_matrix = self.matrix_string.split('\n')
-        res = [row.split(' ')[index-1] for row in list_matrix]
+        res = [row.split(' ')[index-1] for row in self.matrix_string_list]
         return [int(num) for num in res]
 
 
