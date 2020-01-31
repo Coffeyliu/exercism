2f32cc3520c1ba95c2af1e9e923f458fa85a9eaa
Coffeyliu <taliu@wiley.com>
2020-01-31 12:15:48 -0500

    second submission of matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,34 +1,24 @@
-# FIXME: no class docstrings? no method docstrings? :-)
 class Matrix:
+    """Given a matrix, and be able to get row or columns elements by calling method
+        row() or column()
+    """
+
     def __init__(self, matrix_string):
-        # FIXME: well, there are multiple ways to represent a matrix
-        # each of which is optimized for some particular usage while
-        # being relatively weak for other types of usage. In this case,
-        # you are told nothing about the intended usage, so you can choose,
-        # but the representation you chose is a very very unlikely to be
-        # optimal for something. Since this is a numerical matrix, one would
-        # expect that one keeps calling its methods again and again after
-        # instantiation. But if you keep your matrix as a list of strings (rows),
-        # you will have to keep parsing the strings again and again for each call to
-        # row() and column(). That is wasteful. Why not parse it fully the only
-        # once in the constructor __init__() and avoid calling split() and int()
-        # multiple times on every method call?
-        self.matrix_string_list = matrix_string.split("\n")
+        """
+        Parameters
+        ----------
+        matrix_string : str
+            string with all elements for matrix, seperated by '\n' to indicate a 
+            different row and seperated by space to indicate elements in each row.
+            eg: "1 2 3 4\n5 6 7 8\n9 8 7 6"
+        """
+        matrix_list = [row.split(" ") for row in matrix_string.split("\n")]
+        self.matrix_list = [[int(num) for num in row] for row in matrix_list]
 
     def row(self, index):
-        res = self.matrix_string_list[index - 1].split(" ")
-
-        return [int(num) for num in res]
+        """call for row elements showing as a list according to row number"""
+        return self.matrix_list[index - 1]
 
     def column(self, index):
-        # FIXME: for small matrices, it doesn't make much of a difference, but
-        # there is no note of what dimensions the input matrix will have: as the
-        # matrix is larger, the cost of iterating twice through as you
-        # do here becomes noticeable (once to build res, and then to convert it to int).
-        # However, I advice you fix __init__ first before you start on this one (who knows?
-        # you might not need to fix this after all :-) ).
-        res = [row.split(" ")[index - 1] for row in self.matrix_string_list]
-
-        return [int(num) for num in res]
-
-# KUDOS: I really liked that your code can handle non-square matrices. Good job.
+        """call for column elements showing as a list according to row number"""
+        return [row[index - 1] for row in self.matrix_list]
