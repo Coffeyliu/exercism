28661ef56bed653073bd2529f8e06bbf92631b75
Coffeyliu <taliu@wiley.com>
2020-01-31 12:42:34 -0500

    modify __init__ into one line

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -12,9 +12,8 @@ class Matrix:
             different row and seperated by space to indicate elements in each row.
             eg: "1 2 3 4\n5 6 7 8\n9 8 7 6"
         """
-        matrix_list = [row.split(" ") for row in matrix_string.split("\n")]
-        self.matrix_list = [[int(num) for num in row] for row in matrix_list]
-
+        self.matrix_list = [[int(num) for num in row.split(" ")] for row in matrix_string.split("\n")]
+        
     def row(self, index):
         """call for row elements showing as a list according to row number"""
         return self.matrix_list[index - 1]
