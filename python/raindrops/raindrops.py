ef204c8a848009c1a72963e17a8d78330a826da4
shirouto <28932673+shirouto@users.noreply.github.com>
2020-01-31 15:34:19 +0000

    raindrops: very well... 5/5 :-)

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -7,4 +7,7 @@ def convert(number):
     if number % 7 == 0 :
         result = result + 'Plong'
     
+    # NOTE: again, there is a way to avoid that len() call
+    # since result is either "" of some combination of "Pli*"
+    # return result or str(number)
     return result if len(result) != 0 else str(number)
