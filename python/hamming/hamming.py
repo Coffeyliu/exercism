b6783aa3eb5149b694716d7fab12ed8ff61e095c
shirouto <28932673+shirouto@users.noreply.github.com>
2020-02-21 19:46:36 +0000

    hamming: 3/5

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -5,12 +5,40 @@ def distance(strand_a, strand_b):
     ----------
     strand_a, strand_b : str
     """
+
     if len(strand_a) == len(strand_b):
         count = 0
+        # shirouto: most of the time, if you see something like this in python
+        # you better start looking for other ways: you really do not need
+        # to use indeces to iterate through a sequence in python --- almost
+        # always there are ways around. Why? Because it makes for less
+        # readable code, exposes you to boundary issues (not particularly here)
+        # and most importantly, you are likely to end up with not so efficient code
+        # (and the latter does apply here). How many times have you computed the length
+        # of the string in this code? You already know that is O(n), right? We have talked
+        # about this previously...
+
         for i in range(len(strand_a)):
             if strand_a[i] != strand_b[i]:
                 count += 1
+
         return count
     else:
-        raise ValueError ("length of two inputs should be the same.")
+        raise ValueError("length of two inputs should be the same.")
+
+
+# shirouto: here is one way to approach this task
+
+from itertools import starmap, zip_longest
+
+
+def _cmp(x: str, y: str) -> bool:
+    if x and y:
+        return x != y
+    else:
+        raise ValueError("Failed to compare")
+
+
+def _distance(a: str, b: str) -> int:
+    return sum(starmap(_cmp, zip_longest(a, b)))
 
